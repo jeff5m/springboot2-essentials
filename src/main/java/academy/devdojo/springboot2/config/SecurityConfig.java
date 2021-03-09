cb961a6618c9df6200da66905b77848d81d48a5a
jeff5m <jeffmatheuspereira@gmail.com>
2021-03-09 19:33:59 -0300

    43: Spring Secur pt4 authentica user/pass from db

--- src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
+++ src/main/java/academy/devdojo/springboot2/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package academy.devdojo.springboot2.config;
 
+import academy.devdojo.springboot2.service.DevDojoUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,12 +10,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 @Log4j2
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+@SuppressWarnings("java:S5344")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final DevDojoUserDetailsService devDojoUserDetailsService;
+
     /**
      * Spring Security Filters examples:
      * BasicAuthenticationFilter - base64 encode
@@ -22,6 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      * DefaultLogoutPageGeneratingFilter - generate login and logout pages
      * FilterSecurityInterceptor - check if user is authorized
      * Authentication != Authorization
+     *
      * @param http
      * @throws Exception
      */
@@ -42,14 +48,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        log.info("Password encoded {}", passwordEncoder.encode("test"));
+        log.info("Password encoded {}", passwordEncoder.encode("academy"));
+
         auth.inMemoryAuthentication()
-                .withUser("jeff5m")
-                .password(passwordEncoder.encode("admin"))
+                .withUser("jeff5m2")
+                .password(passwordEncoder.encode("academy"))
                 .roles("USER", "ADMIN")
                 .and()
-                .withUser("devdojo")
-                .password(passwordEncoder.encode("admin"))
+                .withUser("devdojo2")
+                .password(passwordEncoder.encode("academy"))
                 .roles("USER");
+
+        auth.userDetailsService(devDojoUserDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
 }
